4cd4fd362b321977db108b0c67f959397348db2e
Logan-Rose <logan.rose0329@gmail.com>
2020-05-15 22:40:56 -0400

    added scroll on button press

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 function show(){
-    console.log("hell")
+    console.log(window.innerHeight)
+    window.scroll({
+        top: window.innerHeight,
+        left: 0,
+        behavior: 'smooth'
+      });
 }
